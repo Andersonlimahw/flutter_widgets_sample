942029e8b8b2fcc74d0bcaa74b0d1378786f96cf
andersonlimadev <andersonlimahw@gmail.com>
2024-03-03 18:08:44 -0300

    💾 [checkpoint] Widgets

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -9,12 +9,139 @@ class SampleItemDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Item Details'),
-      ),
-      body: const Center(
-        child: Text('More Information Here'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Item Details'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 250,
+                    height: 100,
+                    child: const Center(
+                      child: Text(
+                        'Sample Item Details',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 150,
+                    height: 50,
+                    child: const Center(
+                      child: Text(
+                        'Sample Item Details',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 250,
+                    height: 100,
+                    child: const Center(
+                      child: Text(
+                        'Sample Item Details',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 150,
+                    height: 50,
+                    child: const Center(
+                      child: Text(
+                        'Sample Item Details',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                    width: 150,
+                    height: 50,
+                    child: const Center(
+                      child: Text(
+                        'Sample Item Details',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.cyan,
+                    width: 150,
+                    height: 50,
+                    child: const Center(
+                      child: Text(
+                        'Sample Item Details',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.cyan,
+                    width: 150,
+                    height: 50,
+                    child: const Center(
+                      child: Text(
+                        'Sample Item Details',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () => {}, child: const Text('Click me!'))
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
